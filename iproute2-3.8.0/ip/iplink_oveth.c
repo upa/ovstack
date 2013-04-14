f8a1f5280481abb0b901ce7d90afcfa54343f97a
upa <upa@haeena.net>
2013-04-14 19:47:47 +0900

    add iplink_oveth related

---
+++ iproute2-3.8.0/ip/iplink_oveth.c
@@ -0,0 +1,76 @@
+/*
+ * iplink_oveth.c
+ *	overlayed ethernet driver
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <net/if.h>
+#include <linux/ip.h>
+#include <linux/if_link.h>
+#include <arpa/inet.h>
+
+#include "rt_names.h"
+#include "utils.h"
+#include "ip_common.h"
+#include "../../oveth.h"
+
+
+static void explain (void)
+{
+	fprintf (stderr,
+		 "Usage: ... oveth vni VNI\n"
+		 "routing settings is configured by "
+		 "\"ip ov\" and \"ipv oveth\".\n"
+		);
+}
+
+static int 
+oveth_parse_opt (struct link_util * lu, int argc, char ** argv,
+		     struct nlmsghdr * n)
+{
+	__u32 vni;
+	int vni_flag = 0;
+
+	while (argc > 0) {
+		if (!matches (*argv, "help")) {
+			explain ();
+			return -1;
+		} else if (!matches (*argv, "vni")) {
+			NEXT_ARG ();
+			vni = get_u32 (&vni, *argv, 0);
+			vni_flag++;
+		} else {
+			fprintf (stderr, "oveth: unknown command \"%s\"\n",
+				 *argv);
+			return -1;
+		}
+
+		argc--, argv++;
+	}
+
+	if (!vni_flag) {
+		fprintf (stderr, "vni is not specified\n");
+		return -1;
+	}
+
+	addattr32 (n, 1024, IFLA_OVETH_VNI, vni);
+
+	return 0;
+}
+
+
+static void
+oveth_print_opt (struct link_util * lu, FILE * f, struct rtattr *tb[])
+{
+	return;
+}
+
+struct link_util oveth_link_util = {
+	.id		= "oveth",
+	.maxattr	= IFLA_OVETH_MAX,
+	.parse_opt	= oveth_parse_opt,
+	.print_opt	= oveth_print_opt,
+};
